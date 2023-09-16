92f61c6a8db2b1bd8632eb1f39edd8d3bcaaac14
SaurabhMor2000 <115579361+SaurabhMor2000@users.noreply.github.com>
2023-09-16 13:13:56 +0530

    Update database.js

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const { connection } = await mongoose.connect(process.env.MONGO_URI);
+  const { connection } = await mongoose.connect("mongodb+srv://saurabh:ZdOER99mgBQxLPSo@cluster0.wy8es8z.mongodb.net/?retryWrites=true&w=majority");
   console.log(`Mongodb is connected with ${connection.host}`);
 };
